838951340dd3db7070b42ac47b0910378ef3bcf3
Uksusov Kirill <uksusoff@yandex.ru>
2019-08-08 17:28:35 +0400

    Docs added

--- assets/scripts/index.ts
+++ assets/scripts/index.ts
@@ -51,3 +51,8 @@ function onMapClick(e: any) {
 }
 
 map.on('click', onMapClick);
+
+//https://github.com/Luuka/gpx-parser
+//https://stackoverflow.com/questions/28196106/export-gpx-file-from-leaflet
+//https://github.com/tyrasd/togpx
+//https://github.com/shramov/leaflet-plugins/blob/master/examples/yandex.html
