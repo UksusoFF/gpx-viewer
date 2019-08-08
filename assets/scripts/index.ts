a412943c2128904804766dfa8d6057913b992132
Uksusov Kirill <uksusoff@yandex.ru>
2019-08-08 19:49:17 +0400

    Event type added

--- assets/scripts/index.ts
+++ assets/scripts/index.ts
@@ -19,7 +19,10 @@ let yandex = L.yandex();
 
 
 let map = L.map('map', {
-    layers: [yandex],
+    layers: [
+        osm,
+        yandex
+    ],
 }).setView([51.505, -0.09], 13);
 
 var baseMaps = {
@@ -28,7 +31,6 @@ var baseMaps = {
 };
 
 L.control.layers(baseMaps).addTo(map);
-
 
 map.setView(new L.LatLng(51.505, -0.09), 13);
 
@@ -44,27 +46,18 @@ var circle = new L.Circle(new L.LatLng(51.508, -0.11), 500, {
     fillOpacity: 0.5
 }).addTo(map).bindPopup("I am a circle.");
 
-// add polygon
-var latlongs: L.LatLng[];
-latlongs = [
-    new L.LatLng(51.509, -0.08),
-    new L.LatLng(51.503, -0.06),
-    new L.LatLng(51.51, -0.047)
-];
-var polygon = new L.Polygon(latlongs).addTo(map).bindPopup("I am a polygon.");
-
 // popup on mapclick
 var popup = new L.Popup();
 
-function onMapClick(e: any) {
+map.on('click', function(e: L.LeafletEvent) {
+
+    let ee = e as L.LocationEvent;
 
     popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
+        .setLatLng(ee.latlng)
+        .setContent("You clicked the map at " + ee.latlng.toString())
         .openOn(map);
-}
-
-map.on('click', onMapClick);
+});
 
 //https://github.com/mpetazzoni/leaflet-gpx
 //https://github.com/Luuka/gpx-parser
